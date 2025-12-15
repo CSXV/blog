7ff18d0e852dcb24f093bbe29d8ce1fa7c3a76b4
CSXV <maliksharif@hotmail.com>
2025-12-15 12:34:17 +0300

    init

---
+++ assets/Tip-CFKF5bJS.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C80O_cJe.js";function e({title:s="نصيحة",children:i}){return r.jsxs("details",{children:[r.jsx("summary",{children:s}),i]})}export{e as T};
--- assets/Tip-DSHTml1K.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-B-B-42Jn.js";function e({title:s="نصيحة",children:i}){return r.jsxs("details",{children:[r.jsx("summary",{children:s}),i]})}export{e as T};
