11bd2f19299d330739c2f0794e791c3494ac85c1
CSXV <maliksharif@hotmail.com>
2026-01-03 10:28:20 +0300

    init

---
+++ assets/4-BXDVnxj0.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-JDa1Fws2.js";import{F as r,N as o}from"./NoteHeader-CJW1hZZc.js";function n(){const e=r(4);return t.jsxs(t.Fragment,{children:[t.jsx(o,{timeString:e.date,title:e.title}),t.jsx("p",{children:"قيد الكتابة..."})]})}export{n as default};
