f23153113c02c8ab08d70669d903b1c22a832ad1
CSXV <maliksharif@hotmail.com>
2026-01-03 21:37:59 +0300

    init

---
+++ assets/4-CJtzcPil.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DOq07xeB.js";import{F as r,N as o}from"./NoteHeader-DKbq-__w.js";function n(){const e=r(4);return t.jsxs(t.Fragment,{children:[t.jsx(o,{timeString:e.date,title:e.title}),t.jsx("p",{children:"قيد الكتابة..."})]})}export{n as default};
